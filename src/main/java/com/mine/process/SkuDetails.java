ce04c2e8fe406525c9c2f2a1fc992079518644fa
Manjula <manjula.rangu@gmail.com>
2021-12-01 00:39:12 +0530

    format changes

--- src/main/java/com/mine/process/SkuDetails.java
+++ src/main/java/com/mine/process/SkuDetails.java
@@ -1,8 +1,7 @@
 package com.mine.process;
 
-
 /**
- * @author Manjula
+ * @author Manjula 
  * SKU details - model
  *
  */
@@ -14,52 +13,65 @@ public class SkuDetails {
 	private int promoPrice;
 	private String comboId;
 	private int comboPrice;
+
 	public SkuDetails(int count, int price) {
 		super();
 		this.count = count;
 		this.price = price;
 	}
+
 	public int getCount() {
 		return count;
 	}
+
 	public void setCount(int count) {
 		this.count = count;
 	}
+
 	public int getPrice() {
 		return price;
 	}
+
 	public void setPrice(int price) {
 		this.price = price;
 	}
+
 	public int getPromoCount() {
 		return promoCount;
 	}
+
 	public void setPromoCount(int promoCount) {
 		this.promoCount = promoCount;
 	}
+
 	public int getPromoPrice() {
 		return promoPrice;
 	}
+
 	public void setPromoPrice(int promoPrice) {
 		this.promoPrice = promoPrice;
 	}
+
 	public String getComboId() {
 		return comboId;
 	}
+
 	public void setComboId(String comboId) {
 		this.comboId = comboId;
 	}
+
 	public int getComboPrice() {
 		return comboPrice;
 	}
+
 	public void setComboPrice(int comboPrice) {
 		this.comboPrice = comboPrice;
 	}
+
 	@Override
 	public String toString() {
 		return "SkuDetails [count=" + count + ", price=" + price + ", promoCount=" + promoCount + ", promoPrice="
 				+ promoPrice + ", comboId=" + comboId + ", comboPrice=" + comboPrice + "]";
 	}
-	
 
 }
