215cb9a5688e4458fe30aa7d4f8a5cf34684f17a
Manjula <manjula.rangu@gmail.com>
2021-12-01 00:31:56 +0530

    asignment commit

---
+++ src/main/java/com/mine/process/SkuProcessor.java
@@ -0,0 +1,30 @@
+package com.mine.process;
+
+import java.util.HashMap;
+
+public class SkuProcessor {
+public int processTotal(HashMap<String, SkuDetails> cart) {
+	int total=0;
+	for(String id:cart.keySet()) {
+		SkuDetails sku=cart.get(id);
+		int count=sku.getCount();
+		if(sku.getComboId()!=null) {
+			SkuDetails combosku=cart.get(sku.getComboId());
+			int comboCount=combosku.getCount();
+			combosku.setCount(0);
+			cart.put(sku.getComboId(), combosku);
+			total=total+sku.getCount()*sku.getComboPrice();
+		}else if(sku.getPromoCount()>0){
+			//apply promo
+			
+			total=total+sku.getPromoPrice()*(count/sku.getPromoCount());
+			total=total+sku.getPrice()*(count%sku.getPromoCount());
+		}else {
+			total=total+sku.getPrice()*(count);
+		}
+	}
+	return total;
+	
+}
+
+}
---
+++ src/test/java/com/mine/process/SkuProcessorTest.java
@@ -0,0 +1,59 @@
+package com.mine.process;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import java.util.HashMap;
+
+import org.junit.jupiter.api.Test;
+
+public class SkuProcessorTest {
+	@Test
+	public void test1() {
+		HashMap<String, SkuDetails> cart = new HashMap<>();
+		SkuDetails sku = new SkuDetails(1, 50);
+		cart.put("A", sku);
+		SkuDetails sku1 = new SkuDetails(1, 30);
+		cart.put("B", sku1);
+		SkuDetails sku2 = new SkuDetails(1, 20);
+		cart.put("C", sku2);
+		SkuProcessor processor = new SkuProcessor();
+		assertEquals(100, processor.processTotal(cart));
+	}
+
+	@Test
+	public void test2() {
+		HashMap<String, SkuDetails> cart = new HashMap<>();
+		SkuDetails sku = new SkuDetails(5, 50);
+		sku.setPromoCount(3);
+		sku.setPromoPrice(130);
+		cart.put("A", sku);
+		SkuDetails sku1 = new SkuDetails(5, 30);
+		sku1.setPromoCount(2);
+		sku1.setPromoPrice(45);
+		cart.put("B", sku1);
+		SkuDetails sku2 = new SkuDetails(1, 20);
+		cart.put("C", sku2);
+		SkuProcessor processor = new SkuProcessor();
+		assertEquals(370, processor.processTotal(cart));
+	}
+	@Test
+	public void test3() {
+		HashMap<String, SkuDetails> cart = new HashMap<>();
+		SkuDetails sku = new SkuDetails(3, 50);
+		sku.setPromoCount(3);
+		sku.setPromoPrice(130);
+		cart.put("A", sku);
+		SkuDetails sku1 = new SkuDetails(5, 30);
+		sku1.setPromoCount(2);
+		sku1.setPromoPrice(45);
+		cart.put("B", sku1);
+		SkuDetails sku2 = new SkuDetails(1, 20);
+		sku2.setComboId("D");
+		sku2.setComboPrice(30);
+		cart.put("C", sku2);
+		SkuDetails sku3 = new SkuDetails(1, 15);
+		cart.put("D", sku3);
+		SkuProcessor processor = new SkuProcessor();
+		assertEquals(280, processor.processTotal(cart));
+	}
+}
